2d94743d2297f766db07870ecb6891072be5acfd
stathis <lempeg7@gmail.com>
2019-01-02 21:41:44 +0200

    Returns tuples retweeter x retweeted users.

--- server/src/main/java/com/example/ui/Repositories/RetweeterRepository.java
+++ server/src/main/java/com/example/ui/Repositories/RetweeterRepository.java
@@ -15,4 +15,7 @@ public interface RetweeterRepository extends JpaRepository<Retweeter, Long> {
 
     @Query("SELECT DATE(t.timestamp), count(t.retweeter_id) FROM Retweeter t group by DATE(t.timestamp) order by DATE(t.timestamp)")
     List<Object> countByDateRetweetedUserId(@Param("retweeted_user_id") Long retweeted_user_id);
+
+    @Query("select DISTINCT retweeter_id, retweeted_user_id from Retweeter where appUserFlag = ?1")
+    List<Object> getTupleRetweeterRetweeted(@Param("appUserFlag") boolean appUserFlag);
 }
